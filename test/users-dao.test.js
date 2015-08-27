c4154ee368fcf327afbbcf154a83c4cb341142cf
aofry <aofry@yahoo.com>
2015-08-27 10:59:43 +0300

    added user-dao test update

--- test/users-dao.test.js
+++ test/users-dao.test.js
@@ -10,9 +10,18 @@ describe('users-dao', function () {
 
         it('should check save new user', function (done) {
             usersDao.insert({username: "user" + Math.random(), password: "otherpass1"}, function (updatedUser) {
-                console.log("inserted user: " + JSON.stringify(updatedUser));
+                //console.log("inserted user: " + JSON.stringify(updatedUser));
                 if (updatedUser == null)
                     throw new Error('could not insert new user');
+                done();
+            });
+        });
+
+        it('should check update existing user', function (done) {
+            usersDao.update({username: "adi", password: "pass"}, function (updatedUser) {
+                console.log("updated user: " + JSON.stringify(updatedUser));
+                if (updatedUser == null)
+                    throw new Error('could not update user');
                 done();
             });
         });
