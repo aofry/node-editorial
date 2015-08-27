637d7a6c40d89f4b506b377f29283d964c3e74a9
aofry <aofry@yahoo.com>
2015-08-27 11:03:38 +0300

    added dao get  user test

--- test/users-dao.test.js
+++ test/users-dao.test.js
@@ -19,9 +19,18 @@ describe('users-dao', function () {
 
         it('should check update existing user', function (done) {
             usersDao.update({username: "adi", password: "pass"}, function (updatedUser) {
-                console.log("updated user: " + JSON.stringify(updatedUser));
+                //console.log("updated user: " + JSON.stringify(updatedUser));
                 if (updatedUser == null)
                     throw new Error('could not update user');
+                done();
+            });
+        });
+
+        it('should check get user', function (done) {
+            usersDao.get("adi", function (user) {
+                console.log("get user: " + JSON.stringify(user));
+                if (user == null)
+                    throw new Error('could not get user');
                 done();
             });
         });
